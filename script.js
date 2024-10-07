b815c3ccbe714562a5771c1d5c83f3d67499da13
NEHA-PINNAMREDDY <prneha2005@gmail.com>
2024-10-07 17:51:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+// Change the navigation menu style when scrolling
+window.onscroll = function() {
+    const navbar = document.getElementById("navbar");
+    if (window.pageYOffset > 50) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
+    }
+  };
+  
